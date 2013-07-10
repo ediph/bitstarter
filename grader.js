0750ead9d20491847af4aea4999617f1932238f5
Newtosh Ubuntu <ntubuntu@ubuntu.(none)>
2013-07-09 17:31:20 -0700

    updated grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://shrouded-mesa-6336.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +37,21 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};																											
+
+
+var assertURLExists = function(inurl) {
+    var instr = inurl.toString();
+
+	rest.get(instr).on('complete', function(result) {
+	  if (result instanceof Error) {
+        console.log("%s cannot be accessed. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+  		} 
+
+   	  fs.writeFileSync(HTMLFILE_DEFAULT, result);
+	  return HTMLFILE_DEFAULT; //is this needed at all?
+	});																											
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,8 +83,19 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html', clone(assertURLExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+	if (program.file != "")  
+		var f = program.file;
+	else if (program.url != "")
+		var f = HTMLFILE_DEFAULT;
+	else{
+        console.log("No file or URL specified. Exiting.");
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	}
+	
+    var checkJson = checkHtmlFile(f, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
